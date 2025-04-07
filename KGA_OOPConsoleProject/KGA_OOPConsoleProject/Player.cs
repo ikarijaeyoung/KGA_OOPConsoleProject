b355c9deeb5998c44073cb948bb34dce92d2a062
정재영 <wodud04277@gmail.com>
2025-04-07 19:57:44 +0900

    Create Player.cs

---
+++ KGA_OOPConsoleProject/KGA_OOPConsoleProject/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGA_OOPConsoleProject
+{
+    public class Player
+    {
+        private void Move()
+        {
+
+        }
+    }
+}
