cc023e975e113d16f45448bf037d16aba58c1edb
정재영 <wodud04277@gmail.com>
2025-04-07 19:56:06 +0900

    [Feat] start
    게임 반복과 게임 종료 조건을 사용해 게임을 실행

--- KGA_OOPConsoleProject/KGA_OOPConsoleProject/Game.cs
+++ KGA_OOPConsoleProject/KGA_OOPConsoleProject/Game.cs
@@ -8,6 +8,27 @@ namespace KGA_OOPConsoleProject
 {
     public class Game
     {
+        private static bool gameOver;
+        public static void Run()
+        {
+            Start();
 
+            while(gameOver == false)
+            {
+                Scene.Render();
+                Scene.Input();
+                Scene.Update();
+            }
+        }
+
+        public static void Start()
+        {
+            gameOver = false;
+        }
+
+        public static void End()
+        {
+
+        }
     }
 }
